11e576294d973b06e1f55147d18f4537d7c2c91f
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-05-25 11:10:24 -0400

    Creado proyecto,carpeta. falta insertar y hay errores

---
+++ SegundoParcialValeriaTapiaFusco/App2/App2/Interfaces/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Interfaces
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion        
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ SegundoParcialValeriaTapiaFusco/App2/App2/Models/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Notes
+    {
+        [Key]
+        public int NotesId { get; set; }
+        public string Contents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Notes()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+
+    }
+}
